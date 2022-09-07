5036719c447b487b36f5d67cef55d9b70c2fcd40
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-09-07 17:49:10 +0200

    Fixed Gradle Errors

--- app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,15 +1,11 @@
 package com.udacity.project4.locationreminders
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import com.udacity.project4.R
-import kotlinx.android.synthetic.main.activity_reminders.*
+
 
 /**
  * The RemindersActivity that holds the reminders fragments
@@ -22,7 +18,7 @@ class RemindersActivity : AppCompatActivity() {
 
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    /*override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
                 (nav_host_fragment as NavHostFragment).navController.popBackStack()
@@ -30,5 +26,5 @@ class RemindersActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
-    }
+    }*/
 }
