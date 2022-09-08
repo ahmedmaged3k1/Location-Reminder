d640df22fa49a22660a31cab0f75d1dcaf30e733
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-09-08 02:17:55 +0200

    FireBase Authentication

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -6,13 +6,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
-import com.udacity.project4.databinding.ActivityAuthenticationBinding.inflate
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -21,29 +22,35 @@ import com.udacity.project4.databinding.ActivityAuthenticationBinding.inflate
 class AuthenticationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthenticationBinding
+
+    private val viewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthenticationBinding.inflate(layoutInflater)
         val view: View = binding.root
+
         setContentView(view)
+
 //         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
         binding.getStartedButton.setOnClickListener { launchSignInFlow() }
 //          TODO: If the user was authenticated, send him to RemindersActivity
+
 
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        observeAuthenticationState()
+    }
+
     private fun launchSignInFlow() {
 
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
-
-
         )
-
-
         startActivityForResult(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
@@ -52,13 +59,20 @@ class AuthenticationActivity : AppCompatActivity() {
             1001
         )
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 1001) {
             val response = IdpResponse.fromResultIntent(data)
             if (resultCode == Activity.RESULT_OK) {
+
                 // User successfully signed in
-                Log.d(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+                val intent = Intent(this, RemindersActivity::class.java)
+                startActivity(intent)
+                Log.d(
+                    TAG,
+                    "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                )
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
@@ -66,5 +80,38 @@ class AuthenticationActivity : AppCompatActivity() {
                 Log.d(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
             }
         }
+    }
+
+    private fun observeAuthenticationState() {
+
+
+        viewModel.authenticationState.observe(this, Observer { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    binding.textView.text =
+                        "Welcome" + FirebaseAuth.getInstance().currentUser?.displayName
+                    Log.d(
+                        TAG,
+                        "observeAuthenticationState: ${FirebaseAuth.getInstance().currentUser?.displayName}"
+                    )
+                    binding.getStartedButton.text = "Sign Out"
+                    binding.getStartedButton.setOnClickListener {
+                        AuthUI.getInstance().signOut(this)
+                    }
+
+                }
+
+                else -> {
+                    binding.textView.text = "Welcome"
+
+                    binding.getStartedButton.text = "Login"
+                    binding.getStartedButton.setOnClickListener {
+                        launchSignInFlow()
+
+                    }
+
+                }
+            }
+        })
     }
 }
