9de05227e35d5b68426255211909a3200497ebd6
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-09-08 15:30:14 +0200

    Location permission checkpoint

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,15 +1,21 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
 
+import android.Manifest
+import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.dynamic.SupportFragmentWrapper
-import com.google.android.gms.location.*
+import androidx.navigation.findNavController
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.*
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
@@ -17,12 +23,15 @@ import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
-class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
+
+class SelectLocationFragment : BaseFragment(), OnMapReadyCallback,
+    ActivityCompat.OnRequestPermissionsResultCallback {
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
+
     private lateinit var binding: FragmentSelectLocationBinding
-    private lateinit var mMap : GoogleMap
+    private lateinit var mMap: GoogleMap
     private var mapReady = false
 
 
@@ -39,13 +48,53 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         setDisplayHomeAsUpEnabled(true)
 
 //        TODO: add the map setup implementation
-        val mapFragment =childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync{
-            it->mMap =it
-            mapReady=true
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync { it ->
+            mMap = it
+            mapReady = true
             updateMap()
 
         }
+
+        if (ContextCompat.checkSelfPermission(
+                this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this.requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                ActivityCompat.requestPermissions(
+                    this.requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
+                );
+            } else {
+                ActivityCompat.requestPermissions(
+                    this.requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1
+                );
+            }
+        }
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                // Do if the permission is granted
+                Toast.makeText(
+                    this.requireContext(),
+                    "Permission Granted",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            else {
+                // Do otherwise
+                Toast.makeText(this.requireContext(), "Permission Denied", Toast.LENGTH_SHORT)
+                    .show()
+                view?.findNavController()
+                    ?.navigate(R.id.action_selectLocationFragment_to_saveReminderFragment)
+            }
+        }
+
 //        TODO: zoom to the user location after taking his permission
 //        TODO: add style to the map
 //        TODO: put a marker to location that the user selected
@@ -56,7 +105,40 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
         return binding.root
     }
-    private fun updateMap () {
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        Log.d(TAG, "onRequestPermissionsResult: called ")
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(
+                            this.requireContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Toast.makeText(
+                            this.requireContext(),
+                            "Permission Granted",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else {
+                    Toast.makeText(this.requireContext(), "Permission Denied", Toast.LENGTH_SHORT)
+                        .show()
+                    view?.findNavController()
+                        ?.navigate(R.id.action_selectLocationFragment_to_saveReminderFragment)
+                }
+                return
+            }
+        }
+    }
+
+    private fun updateMap() {
 
     }
 
@@ -74,21 +156,21 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         // TODO: Change the map type based on the user's selection.
         R.id.normal_map -> {
-            mMap.mapType=GoogleMap.MAP_TYPE_NORMAL
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
             true
         }
         R.id.hybrid_map -> {
-            mMap.mapType=GoogleMap.MAP_TYPE_HYBRID
+            mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
 
             true
         }
         R.id.satellite_map -> {
-            mMap.mapType=GoogleMap.MAP_TYPE_SATELLITE
+            mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
 
             true
         }
         R.id.terrain_map -> {
-            mMap.mapType=GoogleMap.MAP_TYPE_TERRAIN
+            mMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
 
             true
         }
