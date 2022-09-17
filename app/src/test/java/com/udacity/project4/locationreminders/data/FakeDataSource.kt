7503b19711fb27b287206c879a598331887fa081
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-09-18 01:26:42 +0200

    Fixed Fake Data Source

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,68 +4,36 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource (private var remindersList : MutableList<ReminderDTO>) : ReminderDataSource {
 
     //    TODO: Create a fake data source to act as a double to the real data source
-    var remindersList = mutableListOf<ReminderDTO>()
+
+
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        try {
-
-
-            return Result.Success(remindersList)
-
-        }
-        catch (ex: Exception) {
-            return Result.Error(ex.localizedMessage)
-
-        }
+        remindersList?.let { return Result.Success(it) }
+        return Result.Error(
+            "No Reminders Found "
+        )
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-
-        try {
-
-            if (reminder!=null)
-            {
-                remindersList.add(reminder)
-
-            }
-
-        }
-        catch (ex: Exception) {
-             Result.Error(ex.localizedMessage)
-
-        }
+            remindersList.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-
-        try {
             remindersList.forEach{
                 if (it.id==id)
                     return Result.Success(it)
             }
             return Result.Error("Cannot Found The Reminder With Id $id")
 
-        }
-        catch (ex: Exception) {
-            return Result.Error(ex.localizedMessage)
 
-        }
 
     }
 
     override suspend fun deleteAllReminders() {
-        try {
-
             remindersList.clear()
 
-
-        }
-        catch (ex: Exception) {
-             Result.Error(ex.localizedMessage)
-
-        }
     }
 
 
